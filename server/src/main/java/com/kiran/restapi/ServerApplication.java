3db3059fe6d83eca0bf39d1679056a7303e99ae2
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2024-02-04 23:24:51 +0530

    server runing

--- server/src/main/java/com/kiran/restapi/ServerApplication.java
+++ server/src/main/java/com/kiran/restapi/ServerApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlacementmanagmentApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlacementmanagmentApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
-
 }
